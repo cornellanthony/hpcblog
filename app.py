b8058e83a99fdb2752180ab44080151e17137bc8
Anthony <antcorne@3c22fb3ed556.ant.amazon.com>
2021-06-08 11:42:32 -0500

    added app step function

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 
+CODECOMMIT_REPO_NAME = "pipeline"
+
 from aws_cdk import core
 
-from cdk.cdk_stack import CdkStack
-
+from pipeline.pipeline_stack import PipelineStack
+from pipeline.lambda_stack import LambdaStack
 
 app = core.App()
-CdkStack(app, "cdk")
+
+lambda_stack = LambdaStack(app, "LambdaStack")
+
+PipelineStack(app, "PipelineDeployingLambdaStack",
+    lambda_code=lambda_stack.lambda_code,
+    repo_name=CODECOMMIT_REPO_NAME)
 
 app.synth()
